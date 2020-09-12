c335ae00cea5344ee7041d824d6f23b2ab856862
julianjupiter <julianjupiter.io@gmail.com>
2020-09-12 16:37:08 +0800

    Add initial project structure of product service.

---
+++ storee-product-service/src/main/java/com/julianjupiter/storee/product/StoreeProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.julianjupiter.storee.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreeProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreeProductServiceApplication.class, args);
+	}
+
+}
